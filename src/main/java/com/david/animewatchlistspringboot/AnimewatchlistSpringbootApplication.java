e6618e78ab11b37fe5856796b64fb98c69a91543
David <daveedtee5@gmail.com>
2026-03-17 13:32:05 -0400

    init springboot

---
+++ src/main/java/com/david/animewatchlistspringboot/AnimewatchlistSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.david.animewatchlistspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimewatchlistSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimewatchlistSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/david/animewatchlistspringboot/AnimewatchlistSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.animewatchlistspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimewatchlistSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
