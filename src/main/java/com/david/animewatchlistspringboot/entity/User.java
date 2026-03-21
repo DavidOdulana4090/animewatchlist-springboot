e12e9bb387c1ea22c93c41423c7a64cf4424ef14
David <daveedtee5@gmail.com>
2026-03-21 00:02:15 -0400

    setup for local dev

---
+++ src/main/java/com/david/animewatchlistspringboot/entity/User.java
@@ -0,0 +1,28 @@
+package com.david.animewatchlistspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User(long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
