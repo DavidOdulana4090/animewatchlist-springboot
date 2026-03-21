f34cd1a2efa212c963e6befbcf39877887d5dc18
David <daveedtee5@gmail.com>
2026-03-20 22:33:32 -0400

    getting started

---
+++ src/main/java/com/david/animewatchlistspringboot/controller/ReponseController.java
@@ -0,0 +1,15 @@
+package com.david.animewatchlistspringboot.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+public class ReponseController {
+
+    @RequestMapping("/api/response")
+    public String getResponse(){
+        return "Hello World";
+    }
+}
